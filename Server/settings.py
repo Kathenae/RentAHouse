f222625cb264e61a82d24440590154c18e59af18
Ronald <RonaldWilsonMachado@gmail.com>
2021-07-09 18:01:05 +0200

    Added settings for deplyoing to heroku
    add static files settings for use with whitenoise
    and database settings

--- Server/settings.py
+++ Server/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -121,10 +122,14 @@ USE_TZ = True
 
 # STATIC FILES (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/' 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static'
+    os.path.join(PROJECT_ROOT, 'static'),
 ]
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # AUTH URL SETTINGS
 LOGIN_URL = 'login'
@@ -137,3 +142,7 @@ MEDIA_URL = "/files/"
 
 # CRISPY SETTINGS
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+import dj_database_url 
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
